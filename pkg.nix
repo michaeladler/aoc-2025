dd2d5fc4664aa37b42be7430a85ef20edd18c377
Michael Adler <therisen06@gmail.com>
2025-12-04 11:16:52 +0100

    add matrix package

--- pkg.nix
+++ pkg.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, attoparsec, base, containers, hspec, hspec-discover
-, lib, text
+{ mkDerivation, attoparsec, base, hspec, hspec-discover, lib
+, matrix, text
 }:
 mkDerivation {
   pname = "aoc2025";
@@ -7,9 +7,9 @@ mkDerivation {
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ attoparsec base containers text ];
-  executableHaskellDepends = [ attoparsec base containers text ];
-  testHaskellDepends = [ attoparsec base containers hspec text ];
+  libraryHaskellDepends = [ attoparsec base matrix text ];
+  executableHaskellDepends = [ attoparsec base matrix text ];
+  testHaskellDepends = [ attoparsec base hspec matrix text ];
   testToolDepends = [ hspec-discover ];
   license = lib.licenses.mit;
   mainProgram = "aoc2025";
