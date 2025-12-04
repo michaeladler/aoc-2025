6eb4ce56064e02d67b5111b4c3b57f83291ffea6
Michael Adler <therisen06@gmail.com>
2025-12-04 23:10:06 +0100

    Day 4: refactor
    Use HashSet instead of HashMap

--- src/Day04.hs
+++ src/Day04.hs
@@ -3,20 +3,16 @@ module Day04 where
 import Control.Applicative ((<|>))
 import Data.Attoparsec.Text (Parser, endOfLine, many1, sepBy)
 import qualified Data.Attoparsec.Text as AP
-import qualified Data.HashMap.Strict as HM
-import Data.Maybe (mapMaybe)
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as HS
 import Data.Text (Text)
 
 type MyInt = Int
 
-data Cell = Paper | Empty
-  deriving (Eq)
+data Cell = Empty | Paper
+  deriving (Eq, Show)
 
-instance Show Cell where
-  show Paper = "@"
-  show Empty = "."
-
-type Grid = HM.HashMap (Int, Int) Cell -- first int is row, second is col
+type Grid = HashSet (Int, Int) -- first int is row, second is col
 
 solve :: Text -> Either String (MyInt, MyInt)
 solve input = case parseInput input of
@@ -35,36 +31,44 @@ parseCell = (AP.char '.' >> pure Empty) <|> (AP.char '@' >> pure Paper)
 solveInternal :: [[Cell]] -> (MyInt, MyInt)
 solveInternal input =
   let grid = cellsToGrid input
-      canBeRemoved k = length (filter (== Paper) (adjacentPositions grid k)) < 4
-      part1 = HM.foldlWithKey' f 0 grid
-      f acc k cell = case cell of
-        Empty -> acc
-        Paper -> if canBeRemoved k then acc + 1 else acc
+      canBeRemoved k = adjacentPositions grid k < 4
+      part1 = HS.foldl' f 0 grid
+      f acc k = if canBeRemoved k then acc + 1 else acc
    in (part1, 0)
 
 -- collect neighbors from all adjacent positions (up to 8)
-adjacentPositions :: Grid -> (Int, Int) -> [Cell]
+adjacentPositions :: Grid -> (Int, Int) -> Int
 adjacentPositions grid (row, col) =
-  mapMaybe
-    (`HM.lookup` grid)
-    [ (row - 1, col - 1),
-      (row - 1, col),
-      (row - 1, col + 1),
-      (row, col - 1),
-      (row, col + 1),
-      (row + 1, col - 1),
-      (row + 1, col),
-      (row + 1, col + 1)
-    ]
-
--- Experimental Area
+  length $
+    filter
+      id
+      ( map
+          (`HS.member` grid)
+          [ (row - 1, col - 1),
+            (row - 1, col),
+            (row - 1, col + 1),
+            (row, col - 1),
+            (row, col + 1),
+            (row + 1, col - 1),
+            (row + 1, col),
+            (row + 1, col + 1)
+          ]
+      )
 
 cellsToGrid :: [[Cell]] -> Grid
 cellsToGrid cells =
   let zipped = zip (map (\ys -> zip ys [1 ..]) cells) [1 ..] :: [([(Cell, Int)], Int)] -- first int is col, second int is row
-   in HM.fromList $ concatMap f zipped
+      tmp = concatMap f zipped
+      tmp' = filter (\(_, c) -> isPaper c) tmp
+   in HS.fromList $ map fst tmp'
   where
     f (xs, row) = map (\(cell, col) -> ((row, col), cell)) xs
+
+isPaper :: Cell -> Bool
+isPaper Paper = True
+isPaper _ = False
+
+-- Experimental Area
 
 exampleInput :: Text
 exampleInput =
