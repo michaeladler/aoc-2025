5cbe441bcf68b105e7e1c23b2c3b0aeb4eb3e33e
Michael Adler <therisen06@gmail.com>
2025-12-11 08:31:34 +0100

    chore: update template

--- template/DayXXSpec.hs
+++ template/DayXXSpec.hs
@@ -1,6 +1,5 @@
 module DayXXSpec (spec) where
 
-import qualified Data.ByteString.Char8 as C
 import DayXX (solve)
 import Test.Hspec
 
@@ -9,13 +8,12 @@ spec = do
   describe "solve" $ do
     it "should solve the example" $ do
       solve exampleInput `shouldBe` Right (0, 0)
+  where
+    exampleInput =
+      """
+      987654321111111
+      811111111111119
+      234234234234278
+      818181911112111 
 
-exampleInput :: C.ByteString
-exampleInput =
-  """
-  987654321111111
-  811111111111119
-  234234234234278
-  818181911112111 
-
-  """
+      """
