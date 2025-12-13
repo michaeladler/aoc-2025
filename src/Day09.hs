cfe544053bcb3e5d463a7646c1c7ef6050c4180a
Michael Adler <therisen06@gmail.com>
2025-12-13 12:40:39 +0100

    Ray casting

--- src/Day09.hs
+++ src/Day09.hs
@@ -2,14 +2,17 @@ module Day09 where
 
 import qualified Data.ByteString.Char8 as C
 import Data.Int (Int64)
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, mapMaybe)
 import Debug.Trace (trace)
 
 type MyInt = Int64
 
 type Point = (MyInt, MyInt)
 
-type Line = (Point, Point)
+data Line
+  = Horizontal (Point, MyInt)
+  | Vertical (Point, MyInt)
+  deriving (Eq, Show)
 
 type Polygon = [Line]
 
@@ -27,10 +30,17 @@ inputParser bs = map parsePoint (filter (not . C.null) (C.split '\n' bs))
 solveInternal :: [Point] -> (MyInt, MyInt)
 solveInternal tiles =
   let areas = [rectangleArea p q | p <- tiles, q <- tiles, p /= q]
-      polygon = (head tiles, last tiles) : zip tiles (tail tiles)
+      polygonPoints = (head tiles, last tiles) : zip tiles (tail tiles)
+      polygon = map convertPointsToLine polygonPoints
       -- only have to check the borders of the areas if they are inside the green/red area; use "point in polygon algorithm" from one of the previous years :)
       part1 = maximum areas
    in (part1, trace (show polygon) 0)
+
+convertPointsToLine :: (Point, Point) -> Line
+convertPointsToLine ((x, y), (x', y'))
+  | y == y' = Horizontal ((min x x', y), abs (x' - x))
+  | x == x' = Vertical ((x, min y y'), abs (y' - y))
+  | otherwise = error "Not a line"
 
 rectangleArea :: Point -> Point -> MyInt
 rectangleArea (x, y) (x', y') =
@@ -39,16 +49,23 @@ rectangleArea (x, y) (x', y') =
    in dy * dx
 
 pointOnLine :: Point -> Line -> Bool
-pointOnLine (x, y) ((px, py), (qx, qy))
-  | px == qx =
-      let miny = min py qy
-          maxy = max py qy
-       in miny <= y && y <= maxy -- vertical line
-  | py == qy =
-      let minx = min px qx
-          maxx = max px qx
-       in minx <= x && x <= maxx -- horizontal line
-  | otherwise = error "Not a vertical or horizontal line"
+pointOnLine (x, y) (Horizontal ((x', y'), delta)) = y == y' && x' <= x && x <= x' + delta
+pointOnLine (x, y) (Vertical ((x', y'), delta)) = x == x' && y' <= y && y <= y' + delta
+
+-- Ray casting: To determine if a point is inside a polygon, cast a ray from the point and count edge intersections.
+-- An odd count means inside; even means outside.
+pointInPolygon :: Point -> Polygon -> Bool
+pointInPolygon p poly = odd (length (mapMaybe (p `intersectLine`) poly))
+
+-- Given a Point and a Line, compute the intersection point where a horizontal ray starting from the Point meets the Line.
+intersectLine :: Point -> Line -> Maybe Point
+intersectLine (x, y) (Horizontal ((x', y'), delta))
+  | y == y' && x < x' = Just (x', y)
+  | y == y' && x' <= x && x <= x' + delta = Just (x, y)
+  | otherwise = Nothing
+intersectLine (x, y) (Vertical ((x', y'), delta))
+  | x <= x' && y >= y' && y <= y' + delta = Just (x', y)
+  | otherwise = Nothing
 
 -- Experimental Area
 example :: [Point]
